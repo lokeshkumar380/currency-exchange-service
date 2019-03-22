32bd35df06ed925713b36ebec2a53909bb53ada9
loku0516 <lokesh.kumar@netcracker.com>
2019-03-22 17:37:32 +0530

    added eureka naming server

--- currency-exchange-service/src/main/java/com/example/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/example/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -2,8 +2,12 @@ package com.example.microservices.currencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
